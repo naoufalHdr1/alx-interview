49c9839a0bac6278b2b67d73fab4767170d06598
naoufalHdr1 <naoufalhadra96@gmail.com>
2024-10-08 22:36:17 +0100

    Task 0 (finished) : Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened.
+
+    Args:
+    boxes (list of list of int): A list where each index represents a box, and
+    the inner lists represent the keys inside that box.
+
+    Returns:
+    bool: True if all boxes can be opened, False otherwise.
+    """
+
+    # Number of boxes
+    n = len(boxes)
+
+    # A set to track which boxes we have unlocked
+    unlocked_boxes = set([0])
+
+    # A stack to track keys we need to process
+    keys_to_check = [0]
+
+    # While there are still keys to check
+    while keys_to_check:
+        current_box = keys_to_check.pop()
+
+        for key in boxes[current_box]:
+            if key not in unlocked_boxes and key < n:
+                unlocked_boxes.add(key)
+                keys_to_check.append(key)
+
+    return len(unlocked_boxes) == n
